68c311f00eb94c0abe058fac6dfe3743cf1869e5
Maciej-Grzesik <mackeplz61@gmail.com>
2024-03-20 23:19:46 +0100

    Dodanie CreateUserInfoActivity

--- app/src/main/java/com/example/trainmate/LoginActivity.kt
+++ app/src/main/java/com/example/trainmate/LoginActivity.kt
@@ -8,7 +8,6 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.trainmate.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
