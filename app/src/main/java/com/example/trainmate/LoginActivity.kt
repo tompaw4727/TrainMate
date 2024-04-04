d770637e404a1ae8cccbe48e946c2a845b46350b
Maciej-Grzesik <mackeplz61@gmail.com>
2024-04-04 11:19:55 +0200

    Dodanie widoków activity register

--- app/src/main/java/com/example/trainmate/LoginActivity.kt
+++ app/src/main/java/com/example/trainmate/LoginActivity.kt
@@ -1,17 +1,31 @@
 package com.example.trainmate
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.PersistableBundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 
 class LoginActivity : AppCompatActivity() {
+
+    private var loginButton: Button? = null
+    private var username: EditText? = null
+    private var password: EditText? = null
+    private var forgotPassword: TextView? = null
+    private var register: TextView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        username = findViewById(R.id.username_input)
+        password = findViewById(R.id.password_input)
+
+        loginButton = findViewById(R.id.login_btn)
+        forgotPassword = findViewById(R.id.forgot_password_btn)
+        register = findViewById(R.id.register_btn)
+
     }
 }
