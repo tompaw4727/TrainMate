bea98996f0b1facefb9c7bf3647db783901b12b9
tompaw4727 <tompaw4727@gmail.com>
2024-04-04 11:27:19 +0200

    aktualizacja widokow

---
+++ app/src/main/java/com/example/trainmate/CreateUserInfoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.trainmate
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trainmate.R
+
+class CreateUserInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/trainmate/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.trainmate
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
